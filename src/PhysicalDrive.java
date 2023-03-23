f658b56cf73f83364cf7a5960502c1415340eaa6
BT_4N2_16 <samzad7698@bths.edu>
2023-03-23 12:30:21 -0400

    need to finish

--- src/PhysicalDrive.java
+++ src/PhysicalDrive.java
@@ -1,4 +1,12 @@
+import java.util.UUID;
 public class PhysicalDrive {
     String UUID;
-    
+    String name;
+    int space;
+
+    public PhysicalDrive( UUID, String name, int space) {
+        UUID u = UUID.randomUUID();
+        this.name = name;
+        this.space = space;
+    }
 }
---
+++ src/UUIDGenerator.java
@@ -0,0 +1,7 @@
+import java.util.UUID;
+
+public class UUIDGenerator {
+    public UUIDGenerator() {
+        UUID u = UUID.randomUUID();
+    }
+}
